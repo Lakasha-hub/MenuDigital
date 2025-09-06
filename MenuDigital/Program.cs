850919110c3e2fcc0f54bdf2d965034f1188c44a
lauta <lautaromoreira722@gmail.com>
2025-09-06 14:17:47 -0300

    Add Database Injection and secrets config

--- MenuDigital/Program.cs
+++ MenuDigital/Program.cs
@@ -1,11 +1,28 @@
+using Infraestructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Configure appsettings and secrets
+builder.Configuration
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+    .AddUserSecrets<Program>(optional: true)
+    .AddEnvironmentVariables();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database Injection
+var connectionString = builder.Configuration["ConnectionString"];
+builder.Services.AddDbContext<MenuDigitalContext>(options =>
+    // Agregar cadena de conexión en appsettings.json
+    options.UseNpgsql(connectionString)
+);
 
 var app = builder.Build();
 
