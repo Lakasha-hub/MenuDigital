f7304e289b881d772a80bf35434557f99fd15d80
lauta <lautaromoreira722@gmail.com>
2025-09-06 14:22:49 -0300

    Add configuration DbContext

--- Infraestructure/Persistence/MenuDigitalContext.cs
+++ Infraestructure/Persistence/MenuDigitalContext.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Infraestructure.Persistence
 {
-    internal class MenuDigitalContext : DbContext
+    public class MenuDigitalContext : DbContext
     {
         public DbSet<Dish> Dish { get; set; }
         public DbSet<Category> Category { get; set; }
@@ -20,76 +20,118 @@ namespace Infraestructure.Persistence
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Dish Configuration
-            modelBuilder.Entity<Dish>()
-                .ToTable("Dish")
-                .HasOne<Category>(d => d.CategoryDb)
-                .WithMany(c => c.Dishes)
-                .HasForeignKey(d => d.Category);
+            modelBuilder.Entity<Dish>(entity =>
+            {
+                entity.ToTable("Dish");
 
-            modelBuilder.Entity<Dish>()
-                .HasMany<OrderItem>(d => d.OrderItems)
-                .WithOne(oi => oi.DishDb)
-                .HasForeignKey(oi => oi.Dish);
+                entity.HasKey(d => d.DishId);
+                entity.Property(d => d.Name).IsRequired().HasMaxLength(255);
+                entity.Property(d => d.Description).IsRequired().HasColumnType("text");
+                entity.Property(d => d.Available).HasDefaultValue(true);
+                entity.Property(d => d.Price).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(d => d.ImageUrl).IsRequired().HasColumnType("text");
+                entity.Property(d => d.CreateDate)
+                    .HasColumnType("timestamp")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+                entity.Property(d => d.UpdateDate)
+                    .HasColumnType("timestamp")
+                    .HasDefaultValueSql("CURRENT_TIMESTAMP");
+
+                entity.HasOne<Category>(d => d.CategoryDb)
+                      .WithMany(c => c.Dishes)
+                      .HasForeignKey(d => d.Category);
+                entity.HasMany<OrderItem>(d => d.OrderItems)
+                      .WithOne(oi => oi.DishDb)
+                      .HasForeignKey(oi => oi.Dish);
+            });
 
             // Category Configuration
-            modelBuilder.Entity<Category>()
-                .ToTable("Category")
-                .HasMany<Dish>(c => c.Dishes)
-                .WithOne(d => d.CategoryDb)
-                .HasForeignKey(d => d.Category);
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.HasKey(c => c.Id);
+                entity.Property(c => c.Id).ValueGeneratedOnAdd();
+                entity.Property(c => c.Name).IsRequired().HasMaxLength(25);
+                entity.Property(c => c.Description).IsRequired().HasMaxLength(255);
+                entity.Property(c => c.Order).IsRequired().ValueGeneratedOnAdd();
+            });
 
             // Order Configuration
-            modelBuilder.Entity<Order>()
-                .ToTable("Order")
-                .HasMany<OrderItem>(o => o.OrderItems)
-                .WithOne(oi => oi.OrderDb)
-                .HasForeignKey(oi => oi.Order);
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.ToTable("Order");
 
-            modelBuilder.Entity<Order>()
-                .HasOne<DeliveryType>(o => o.DeliveryTypeDb)
-                .WithMany(dt => dt.Orders)
-                .HasForeignKey(o => o.DeliveryType);
+                entity.HasKey(o => o.OrderId);
+                entity.Property(o => o.OrderId).HasColumnType("bigint").ValueGeneratedOnAdd();
+                entity.Property(o => o.DeliveryTo).IsRequired().HasMaxLength(255);
+                entity.Property(o => o.Notes).HasColumnType("text");
+                entity.Property(o => o.Price).IsRequired().HasColumnType("decimal(18,2)");
+                entity.Property(o => o.CreateDate)
+                      .HasColumnType("timestamp")
+                      .HasDefaultValueSql("CURRENT_TIMESTAMP");
+                entity.Property(o => o.UpdateDate)
+                        .HasColumnType("timestamp")
+                        .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
-            modelBuilder.Entity<Order>()
-                .HasOne<Status>(o => o.StatusDb)
-                .WithMany(s => s.Orders)
-                .HasForeignKey(o => o.OverallStatus);
+                entity.HasMany<OrderItem>(o => o.OrderItems)
+                      .WithOne(oi => oi.OrderDb)
+                      .HasForeignKey(oi => oi.Order);
+
+                entity.HasOne<DeliveryType>(o => o.DeliveryTypeDb)
+                        .WithMany(dt => dt.Orders)
+                        .HasForeignKey(o => o.DeliveryType);
+
+                entity.HasOne<Status>(o => o.StatusDb)
+                        .WithMany(s => s.Orders)
+                        .HasForeignKey(o => o.OverallStatus);
+            });
 
             // OrderItem Configuration
-            modelBuilder.Entity<OrderItem>()
-                .ToTable("OrderItem")
-                .HasOne<Dish>(oi => oi.DishDb)
-                .WithMany(d => d.OrderItems)
-                .HasForeignKey(oi => oi.Dish);
+            modelBuilder.Entity<OrderItem>(entity =>
+            {
+                entity.ToTable("OrderItem");
 
-            modelBuilder.Entity<OrderItem>()
-                .HasOne<Order>(oi => oi.OrderDb)
-                .WithMany(o => o.OrderItems)
-                .HasForeignKey(oi => oi.Order);
+                entity.HasKey(oi => oi.OrderItemId);
+                entity.Property(oi => oi.OrderItemId).HasColumnType("bigint").ValueGeneratedOnAdd();
+                entity.Property(oi => oi.Quantity).IsRequired();
+                entity.Property(oi => oi.Notes).HasColumnType("text");
+                entity.Property(oi => oi.CreateDate)
+                      .HasColumnType("timestamp")
+                      .HasDefaultValueSql("CURRENT_TIMESTAMP");
 
-            modelBuilder.Entity<OrderItem>()
-                .HasOne<Status>(oi => oi.StatusDb)
-                .WithMany(s => s.OrderItems)
-                .HasForeignKey(oi => oi.Status);
+                entity.HasOne<Dish>(oi => oi.DishDb)
+                        .WithMany(d => d.OrderItems)
+                        .HasForeignKey(oi => oi.Dish);
+
+                entity.HasOne<Order>(oi => oi.OrderDb)
+                        .WithMany(o => o.OrderItems)
+                        .HasForeignKey(oi => oi.Order);
+
+                entity.HasOne<Status>(oi => oi.StatusDb)
+                        .WithMany(s => s.OrderItems)
+                        .HasForeignKey(oi => oi.Status);
+            });
 
             // DeliveryType Configuration
-            modelBuilder.Entity<DeliveryType>()
-                .ToTable("DeliveryType")
-                .HasMany<Order>(dt => dt.Orders)
-                .WithOne(o => o.DeliveryTypeDb)
-                .HasForeignKey(o => o.DeliveryType);
+            modelBuilder.Entity<DeliveryType>(entity =>
+            {
+                entity.ToTable("DeliveryType");
+
+                entity.HasKey(dt => dt.Id);
+                entity.Property(dt => dt.Id).ValueGeneratedOnAdd();
+                entity.Property(dt => dt.Name).IsRequired().HasMaxLength(25);
+            });
 
             // Status Configuration
-            modelBuilder.Entity<Status>()
-                .ToTable("Status")
-                .HasMany<Order>(s => s.Orders)
-                .WithOne(o => o.StatusDb)
-                .HasForeignKey(o => o.OverallStatus);
+            modelBuilder.Entity<Status>(entity =>
+            {
+                entity.ToTable("Status");
 
-            modelBuilder.Entity<Status>()
-                .HasMany<OrderItem>(s => s.OrderItems)
-                .WithOne(oi => oi.StatusDb)
-                .HasForeignKey(oi => oi.Status);
+                entity.HasKey(s => s.Id);
+                entity.Property(s => s.Id).ValueGeneratedOnAdd();
+                entity.Property(s => s.Name).IsRequired().HasMaxLength(25);
+            });
         }
     }
 }
