fca19fb2db26e389e5c1389e45784e94e243d9e3
lauta <lautaromoreira722@gmail.com>
2025-09-08 21:32:19 -0300

    Add(dish): insert and update

---
+++ Infraestructure/Command/DishCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aplication.Interfaces.Command;
+using Domain.Entities;
+using Infraestructure.Persistence;
+
+namespace Infraestructure.Command
+{
+    public class DishCommand : IDishCommand
+    {
+        private readonly MenuDigitalContext _context;
+
+        public DishCommand(MenuDigitalContext context)
+        {
+            _context = context;
+        }
+
+        public async Task InsertDish(Dish dish)
+        {
+            _context.Add(dish);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateDish(Dish dish)
+        {
+            _context.Update(dish);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
