f27cdfcfbd06ab46d269c2c7b786c4084f4f3a57
lauta <lautaromoreira722@gmail.com>
2025-09-08 21:34:08 -0300

    Add(dish): querys GetbyName, getbyId, GetAll. Add(category): GetbyId

---
+++ Infraestructure/Querys/CategoryQuery.cs
@@ -0,0 +1,27 @@
+﻿using Aplication.Interfaces.Querys;
+using Domain.Entities;
+using Infraestructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Querys
+{
+    public class CategoryQuery : ICategoryQuery
+    {
+        private readonly MenuDigitalContext _context;
+
+        public CategoryQuery(MenuDigitalContext context)
+        {
+            _context = context;
+        }
+
+        public Category GetCategoryById(int id)
+        {
+            var category = _context.Category.FirstOrDefault(c => c.Id == id);
+            return category;
+        }
+    }
+}
---
+++ Infraestructure/Querys/DishQuery.cs
@@ -0,0 +1,38 @@
+﻿using Aplication.Interfaces.Querys;
+using Domain.Entities;
+using Infraestructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Querys
+{
+    public class DishQuery : IDishQuery
+    {
+        private readonly MenuDigitalContext _context;
+
+        public DishQuery(MenuDigitalContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Dish>> GetAllDishes()
+        {
+            return _context.Dish.Include(d => d.CategoryDb);
+        }
+
+        public Dish GetDishByName(string name)
+        {
+            return _context.Dish.FirstOrDefault(d => d.Name == name);
+        }
+
+        public Dish GetDishById(Guid id)
+        {
+            return _context.Dish.FirstOrDefault(d => d.DishId == id);
+        }
+    }
+}
